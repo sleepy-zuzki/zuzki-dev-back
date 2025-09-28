9818883ab0f13b6780160bc8a07bd49b46d9a79e
Zuzki <sleepy@zuzki.dev>
2025-09-28 11:57:54 -0500

    ### Add Public Decorator and Version Metadata to Contact Controller
    Updated the `ContactController` to include a versioning metadata and marked the contact endpoint as public by applying the `@Public()` decorator.
    
    ---
    
    **Global Changes:**
    - Added version information to the `@Controller` decorator to specify the API version as `v1`.
    - Applied the `@Public` decorator to mark the `create` method as publicly accessible, bypassing authentication.
    
    ---
    
    ### List of updated files:
    
    #### `contact.controller`
    - Added `{ path: 'contact', version: '1' }` metadata to the `@Controller` decorator for consistent versioning.
    - Included the `@Public` decorator to ensure the POST `create` method is publicly accessible.

--- src/interfaces/http/v1/contact/controllers/contact.controller.ts
+++ src/interfaces/http/v1/contact/controllers/contact.controller.ts
@@ -7,14 +7,16 @@ import {
   ValidationPipe,
 } from '@nestjs/common';
 
+import { Public } from '@app/auth/decorators/public.decorator';
 import { SubmitContactService } from '@application/contact/services/submit-contact.service';
 
 import { ContactRequestDto } from '../dto/contact-request.dto';
 
-@Controller('contact')
+@Controller({ path: 'contact', version: '1' })
 export class ContactController {
   constructor(private readonly submitContactService: SubmitContactService) {}
 
+  @Public()
   @Post()
   @HttpCode(HttpStatus.NO_CONTENT)
   async create(
